48265e82488a4fe992b824df505f6cff694158ad
Jon Gucciardi <jon.gucciardi@catapultsystems.com>
2020-09-21 11:34:59 -0500

    closures and structs

---
+++ anon_functions/target/debug/deps/anon_functions-9d3fd7dd12520c75.d
@@ -0,0 +1,5 @@
+C:\Users\jgucciardi\Rust\anon_functions\target\debug\deps\anon_functions-9d3fd7dd12520c75.exe: src\main.rs
+
+C:\Users\jgucciardi\Rust\anon_functions\target\debug\deps\anon_functions-9d3fd7dd12520c75.d: src\main.rs
+
+src\main.rs:
---
+++ anon_functions/target/debug/deps/anon_functions-ef58be8554ab3635.d
@@ -0,0 +1,5 @@
+C:\Users\jgucciardi\Rust\anon_functions\target\debug\deps\anon_functions-ef58be8554ab3635.exe: src\lib.rs
+
+C:\Users\jgucciardi\Rust\anon_functions\target\debug\deps\anon_functions-ef58be8554ab3635.d: src\lib.rs
+
+src\lib.rs:
